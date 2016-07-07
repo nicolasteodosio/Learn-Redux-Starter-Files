693d073d682aa45013f92617f4d2ce293300f698
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2016-07-06 23:38:02 -0300

    Creating reducers

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,6 @@
+function comments(state = [], action){
+    console.log(stae, action);
+    return state;
+}
+
+export default comments
---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,8 @@
+//a reducer takes in two things:
+
+//1. the action
+//2. copy of current state
+function posts(state = [], action){
+    console.log(stae, action);
+    return state;
+}
